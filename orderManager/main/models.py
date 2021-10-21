2ee18fb126b68204ab44041d7af54591ceafc7b6
Ariel Kran <admin@squaredcomputing.com>
2021-10-21 16:23:30 -0400

    Make second veiw start database

--- orderManager/main/models.py
+++ orderManager/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class OrderNum(models.Model):
+    IdNum = models.
+
+    def __init__(self, arg):
+        superOrderNum, self).__init__()
+        self.arg = arg
--- orderManager/main/views.py
+++ orderManager/main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(response):
     return HttpResponse("<h1>test</h1>")
+
+def orders(response):
+    return HttpResponse("this will be the order page")
